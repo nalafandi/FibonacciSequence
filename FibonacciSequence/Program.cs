c842facf16ee7184c2b587dcf54ea85f3880fc4f
niafa <niafa@DESKTOP-2V1QPRJ>
2022-09-15 16:34:35 -0400

    Add project files.

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FibonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] farray = new int[25];
+            farray[0] = 0;
+            farray[1] = 1;
+
+            for (int i = 0; i < 25; i++)
+            {
+                if (i >= 2)
+                {
+                    farray[i] = farray[i - 2] + farray[i - 1];
+                }
+                Console.WriteLine(farray[i]);                
+            }
+
+
+        }
+    }
+}
